b9c6d783a0238142d418728c950410a5b3c146d3
Abby Lam <abbyhslam@gmail.com>
2025-07-31 13:05:08 +0100

    changes

--- hrqol_cv/code.sh
+++ hrqol_cv/code.sh
@@ -1,6 +1,6 @@
 #PBS -l walltime=48:00:00
 #PBS -l select=1:ncpus=20:mem=400gb
-#PBS -N gad7_round4
+#PBS -N eq5d_round2
 
 
 cd /rds/general/user/hsl121/home/hda_project/hrqol_cv
@@ -8,4 +8,4 @@ cd /rds/general/user/hsl121/home/hda_project/hrqol_cv
 eval "$(~/anaconda3/bin/conda shell.bash hook)"
 source activate TDS
 
-python gad7.py
+python eq5d.py
